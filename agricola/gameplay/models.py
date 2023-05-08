c90ae94608e3ef6950e0a2191072599abbdc5215
Dongyeon Han <gksehddus202@naver.com>
2023-05-09 06:04:32 +0900

    ADD some models

--- agricola/gameplay/models.py
+++ agricola/gameplay/models.py
@@ -2,7 +2,33 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    email = models.CharField(max_length=30, null=True)
+    email = models.CharField(primary_key=True, max_length=30, null=False, default='-')
     name = models.CharField(max_length=10)
-    user_id = models.CharField(max_length=20, null=True)
-    user_pw = models.CharField(max_length=20, null=True)
+    user_id = models.CharField(max_length=20, null=False)
+    user_pw = models.CharField(max_length=20, null=False)
+
+class Player(models.Model):
+    # id = models.AutoField(primary_key=True) # primary_key 옵션이 없으면 자동으로 추가해준다
+    user_id = models.ForeignKey('Account', on_delete=models.CASCADE)
+    adult_num = models.IntegerField(null=False, default=2)
+    baby_num = models.IntegerField(null=False, default=0)
+    fst_player = models.BooleanField(default=False)
+    score = models.IntegerField(null=False, default=0)
+
+class PlayerBoardStatus(models.Model):
+    player_id = models.ForeignKey('Player', on_delete=models.CASCADE)
+    house = models.CharField(max_length=2, null=False, default="나무")
+    room_num = models.IntegerField(null=False, default=2)
+
+class BoardPosition(models.Model):
+    board_id = models.ForeignKey('PlayerBoardStatus', on_delete=models.CASCADE)
+    position = models.IntegerField(null=False)
+    position_name = models.CharField(max_length=6, null=False, default='빈칸')
+    is_fam = models.BooleanField(null=False, default=False)
+
+class FencePosition(models.Model):
+    board_id = models.ForeignKey('BoardPosition', on_delete=models.CASCADE)
+    left = models.BooleanField(null=False, default=False)
+    top = models.BooleanField(null=False, default=False)
+    right = models.BooleanField(null=False, default=False)
+    bottom = models.BooleanField(null=False, default=False)
--- agricola/gameplay/serializer.py
+++ agricola/gameplay/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-from .models import Account
+from .models import Account, Player, PlayerBoardStatus, BoardPosition, FencePosition
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,5 +10,49 @@ class AccountSerializer(serializers.ModelSerializer):
             UniqueTogetherValidator(
                 queryset=Account.objects.all(),
                 fields=['email', 'name'],
+            )
+        ]
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Player.objects.all(),
+                fields=['id'],
+            )
+        ]
+
+class PlayerBoardStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlayerBoardStatus
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=PlayerBoardStatus.objects.all(),
+                fields=['id'],
+            )
+        ]
+
+class BoardPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BoardPosition
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=BoardPosition.objects.all(),
+                fields=['id'],
+            )
+        ]
+
+class FencePositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FencePosition
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=FencePosition.objects.all(),
+                fields=['id'],
             )
         ]
