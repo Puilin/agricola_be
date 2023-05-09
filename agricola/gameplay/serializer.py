8d68a808d8cd391f612a91a8bde11e62656f1df1
Hong-Yehee <ghddpgml@gmail.com>
2023-05-09 15:40:09 +0900

    commit

--- agricola/gameplay/serializer.py
+++ agricola/gameplay/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-from .models import Account, Player, PlayerBoardStatus, BoardPosition, FencePosition
+from .models import Account
 
 
 class AccountSerializer(serializers.ModelSerializer):
