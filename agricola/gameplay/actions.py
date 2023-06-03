ad0604cbc349fbb53eb775cfd8a382de68780eca
puilin <suryblue@naver.com>
2023-06-04 00:48:50 +0900

    ADD : 로직 추가
    take_action
    - 곡식종자(grain_seed)

--- agricola/gameplay/actions.py
+++ agricola/gameplay/actions.py
@@ -23,3 +23,20 @@ def forest(player):
         return Response(serializer.data)
     else:
         return Response({'detail': 'There\'s no exisiting resources.'}, status=404)
+
+def grain_seed(player):
+    grain_action = ActionBox.objects.get(id=10)
+    my_resource = PlayerResource.objects.get(player_id=player.id, resource_id=5)
+    # 곡식 종자 칸에 누군가 있으면
+    if grain_action.is_occupied:
+        return Response({'detail': 'There\'s someone else in grain seed'}, status=404)
+    # 곡식종자 칸에 이제 사람이 있음
+    grain_action.is_occupied = True
+    grain_action.save()
+
+    # 곡식을 하나 받음
+    my_resource.resource_num += 1
+    my_resource.save()
+
+    serializer = PlayerResourceSerializer(my_resource)
+    return Response(serializer.data)
