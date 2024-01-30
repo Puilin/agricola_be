be3a2416809bd95e7f98e8973b74f9531df9479f
puilin <suryblue@naver.com>
2024-01-31 03:52:02 +0900

    fix : route with r string

--- agricola/gameplay/routing.py
+++ agricola/gameplay/routing.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import consumers
 
 # ws://localhost:8000/ws/agricola1/player_1/
 websocket_urlpatterns = [
-    path('ws/<str:room_name>/player_<str:player_id>/', consumers.Consumer.as_asgi())
+    re_path(r'ws/(?P<room_name>\w+)/player_(?P<player_id>\w+)/', consumers.Consumer.as_asgi())
 ]
