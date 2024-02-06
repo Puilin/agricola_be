bbcd6307f99d7b4a47ba1746abed054bc1ffd29f
puilin <suryblue@naver.com>
2024-02-07 00:33:20 +0900

    fix typos

--- agricola/gameplay/routing.py
+++ agricola/gameplay/routing.py
@@ -1,7 +1,6 @@
 from django.urls import path, re_path
 
 from . import consumers
-from . import test_consumer
 
 websocket_urlpatterns = [
     re_path(r"ws/(?P<room_name>\w+)/(?P<player_id>\w+)", consumers.Consumer.as_asgi()),
