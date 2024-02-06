def90cd8f5d263245aacc0c8dc32e3016c6277ff
puilin <suryblue@naver.com>
2024-02-07 02:26:52 +0900

    fix : middleware

--- agricola/gameplay/broadcast_middleware.py
+++ agricola/gameplay/broadcast_middleware.py
@@ -8,7 +8,7 @@ class BroadcastMiddleware:
 
     def __call__(self, request):
         # API 호출 시에 room_name이라는 필드가 요청에 있다면 추가
-        room_num = request.GET['room_num']
+        room_num = request.GET.get('room_num')
         if room_num:
             request.room_num = room_num
 
