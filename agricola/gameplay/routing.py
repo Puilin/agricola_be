aeec13c31ad6d0f0263335eaf6de5631efb3c5e4
puilin <suryblue@naver.com>
2024-02-06 21:39:17 +0900

    some tests with routing.py

--- agricola/gameplay/routing.py
+++ agricola/gameplay/routing.py
@@ -1,8 +1,8 @@
-from django.urls import re_path
+from django.urls import path, re_path
 
 from . import consumers
 
 websocket_urlpatterns = [
     #re_path(r"ws/(?P<room_name>\w+)/(?P<player_id>\w+)/$", consumers.Consumer.as_asgi()),
-    re_path(r"ws/", consumers.Consumer.as_asgi()),
+    path("ws/<str:room_name>/<str:player_id>/", consumers.Consumer.as_asgi())
 ]
