55444f0bd2bcc4e935116fd5865e8f4ac9f11513
puilin <suryblue@naver.com>
2024-02-06 20:57:00 +0900

    update routing.py

--- agricola/agricola/asgi.py
+++ agricola/agricola/asgi.py
@@ -8,20 +8,28 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+
+from django.urls import path
+
+from agricola.gameplay.consumers import Consumer
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agricola.settings')
 import django
 django.setup()
 
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from gameplay.routing import websocket_urlpatterns
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(), 
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    )
-})
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
--- agricola/gameplay/routing.py
+++ agricola/gameplay/routing.py
@@ -1,8 +1,7 @@
-from django.urls import path, re_path
+from django.urls import re_path
 
 from . import consumers
 
-# ws://localhost:8000/ws/agricola1/player_1/
 websocket_urlpatterns = [
-    re_path(r'ws/(?P<room_name>\w+)/player_(?P<player_id>\w+)/', consumers.Consumer.as_asgi())
+    re_path(r"ws/(?P<room_name>\w+)/(?P<player_id>\w+)/$", consumers.Consumer.as_asgi()),
 ]
