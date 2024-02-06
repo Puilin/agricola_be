ac2e160d869ee2700f66f256c35407c891b7c1e7
puilin <suryblue@naver.com>
2024-02-07 00:11:42 +0900

    url pattern tests

--- agricola/gameplay/routing.py
+++ agricola/gameplay/routing.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
 
 from . import consumers
+from . import test_consumer
 
 websocket_urlpatterns = [
-    #re_path(r"ws/(?P<room_name>\w+)/(?P<player_id>\w+)/$", consumers.Consumer.as_asgi()),
-    path("ws/<str:room_name>/<str:player_id>/", consumers.Consumer.as_asgi())
+    re_path(r"ws/", test_consumer.TestConsumer.as_asgi()),
+    # re_path(r"ws/(?P<room_name>\w+)/(?P<player_id>\w+)/$", consumers.Consumer.as_asgi()),
 ]
