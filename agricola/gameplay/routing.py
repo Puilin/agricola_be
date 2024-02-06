cafad89109688002c86c74061d67c514d45599c3
puilin <suryblue@naver.com>
2024-02-06 21:34:10 +0900

    some tests with routing.py

--- agricola/gameplay/routing.py
+++ agricola/gameplay/routing.py
@@ -3,5 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/(?P<room_name>\w+)/(?P<player_id>\w+)/$", consumers.Consumer.as_asgi()),
+    #re_path(r"ws/(?P<room_name>\w+)/(?P<player_id>\w+)/$", consumers.Consumer.as_asgi()),
+    re_path(r"ws/", consumers.Consumer.as_asgi()),
 ]
