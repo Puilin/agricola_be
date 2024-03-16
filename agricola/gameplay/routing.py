2d7c8a9591e68bc3d4b1867f5d182f2e77e26f7d
puilin <suryblue@naver.com>
2024-03-16 17:59:25 +0900

    fix : url patterns for socket

--- agricola/gameplay/routing.py
+++ agricola/gameplay/routing.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/(?P<room_name>\w+)/(?P<player_id>\w+)", consumers.Consumer.as_asgi()),
+    re_path(r"ws/(?P<room_name>\w+)/(?P<player_id>\w+)/", consumers.Consumer.as_asgi()),
 ]
